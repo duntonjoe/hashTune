032cbb61cc762907a22e1b8ce979a8012ac88ebf
jmdunton <jmdunton@horo.muad.local>
2017-11-09 20:02:36 -0500

    finished

--- hashTune/hashCodes/HashTune.java
+++ hashTune/hashCodes/HashTune.java
@@ -1,5 +1,6 @@
 package hashCodes;
 
+import java.util.Random;
 import java.util.Scanner;
 
 import javax.sound.midi.MidiChannel;
@@ -11,6 +12,7 @@ public class HashTune {
 	static String music;
 	static int volume = 80;
 	static int duration = 300;
+	static int bend = 10;
 	
 	public static void main(String[] args) throws MidiUnavailableException, InterruptedException {
 		System.out.println("Type some string of words/characters, then press enter");
@@ -27,21 +29,37 @@ public class HashTune {
 		Synthesizer synth = MidiSystem.getSynthesizer();
         synth.open();
         MidiChannel[] channels = synth.getChannels();
+        Random rand = new Random();
 		int[] notes = {0, 48, 50, 52, 54, 56, 58, 60, 62};
 		for(char x : music.toCharArray()){
 			int note = Character.getNumericValue(x);
 			if(note == 9){
-				channels[9].noteOn( 38, volume );
+				int  percussion = rand.nextInt(48) + 33;
+				channels[9].noteOn( percussion, volume );
 		        Thread.sleep( duration );
-		        channels[9].noteOff( 38 );
+		        channels[9].noteOff( percussion);
+		        for(int i = 1; i < 8; i++){
+					notes[i] -=3;
+				}
 			}
 			else if (note > 0 && note <= 8 ){
 				channels[0].noteOn( notes[note], volume );
 		        Thread.sleep( duration );
-		        channels[0].noteOff( notes[note] );
+		        channels[0].noteOff( notes[note]);
 			}
 			else if (note == 0){
-				duration *= 1.1;
+				int shift = rand.nextInt(900);
+				if((shift % 2) ==0){
+					for(int i = 1; i < 8; i++){
+						notes[i] +=3;
+					}
+				}
+				else{
+					for(int i = 1; i < 8; i++){
+						notes[i] -=3;
+					}
+				}
+				
 			}
 			System.out.print(x);
 			
